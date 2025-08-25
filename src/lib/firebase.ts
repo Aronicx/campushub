ab2ccd6edba63bc9a9084aa40d9bf4e8d8cb0491
GAURAV YADAV RAJKUMAR <g25.gaurav.yadavrajkumar@gnkhalsa.edu.in>
2025-08-25 17:21:05 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "campus-hub-wmhbw",
-  appId: "1:162991212051:web:456c009133bca5dcd3e1e6",
-  storageBucket: "campus-hub-wmhbw.firebasestorage.app",
-  apiKey: "AIzaSyBNLeAcRUo_Lix-HeVyikDMK_HJW6uDVVM",
-  authDomain: "campus-hub-wmhbw.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "162991212051",
+  "projectId": "campus-hub-wmhbw",
+  "appId": "1:162991212051:web:456c009133bca5dcd3e1e6",
+  "storageBucket": "campus-hub-wmhbw.firebasestorage.app",
+  "apiKey": "AIzaSyBNLeAcRUo_Lix-HeVyikDMK_HJW6uDVVM",
+  "authDomain": "campus-hub-wmhbw.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "162991212051"
 };
 
 // Initialize Firebase
